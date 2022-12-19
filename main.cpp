4f8af84185553d58a1bb4c3deda72cab0dc72b15
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-12-19 13:15:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+float f(float x , float &fx)
+{
+    return fx = x * x * cos(x) + 1;
+}
 
 int main() {
-   return 0;
+float a=0,b=0,fx=0,x=0,err=2;
+
+while(f(a,fx)*f(b,fx)>=0) {
+    cout <<"inserire estremi";
+    cin >> a >> b;
+
+    if (f(a,fx)*f(b,fx) < 0){
+        break;
+    }
 }
+
+while (f(x,fx)!=0) {
+    x=(a+b)/2;
+        if (f(x, fx) * f(b, fx) < 0) {
+            a = x;
+        }
+        else{
+        b = x;
+        }
+    err = abs((a - b)) / 2;
+        if (err < 1e-6){
+            break;}
+}
+cout.precision(4);
+cout << x<<endl;
+cout.precision(4);
+cout << f(x,fx);
+
+
+return 0;
+
+
+
+}
+
